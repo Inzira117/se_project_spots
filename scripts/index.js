89585bcc8982759afb1eb83228be30931423b329
Inzira <inzira1995@gmail.com>
2024-11-08 13:56:24 -0800

    Project 6 sprint 6

--- scripts/index.js
+++ scripts/index.js
@@ -35,11 +35,13 @@ const initialCards = [
   },
 ];
 
+// Profile elements
 const profileModalOpenButton = document.querySelector(".profile__edit-btn");
 const cardModalOpenButton = document.querySelector(".profile__add-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
+// Edit form elements
 const editModal = document.querySelector("#edit-modal");
 const editFormElement = editModal.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
@@ -48,11 +50,14 @@ const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
 
+// Card form elements
 const cardModal = document.querySelector("#add-card-modal");
+const cardSubmitBtn = cardModal.querySelector(".modal__submit-btn");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 
+// Previev image elements
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
@@ -60,6 +65,7 @@ const previevCloseBtn = previewModal.querySelector(
   ".modal__close-btn_type_preview"
 );
 
+// Card related elements
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
 const cardFormElement = cardModal.querySelector(".modal__form");
@@ -126,11 +132,13 @@ function handleCardFormSubmit(evt) {
   cardNameInput.value = "";
   cardLinkInput.value = "";
   evt.target.reset();
+  disabledButton(cardSubmitBtn, settings);
 }
 
 profileModalOpenButton.addEventListener("click", () => {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
+  resetValidation(editModal, [editModalNameInput, editModalDescriptionInput]);
   openModal(editModal);
 });
 
---
+++ scripts/validation.js
@@ -0,0 +1,82 @@
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__submit-btn",
+  inactiveButtonClass: "modal__submit-btn-disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+const showInputError = (formElement, inputElement, errorMsg, config) => {
+  const errorMsgID = inputElement.id + "-error";
+  const errorMsgEl = formElement.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = errorMsg;
+  inputElement.classList.add(config.inputErrorClass);
+};
+
+const hideInputError = (formElement, inputElement, config) => {
+  const errorMsgID = inputElement.id + "-error";
+  const errorMsgEl = formElement.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = "";
+  inputElement.classList.remove(config.inputErrorClass);
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList)) {
+    disabledButton(buttonElement, config);
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(config.inactiveButtonClass);
+  }
+};
+
+const disabledButton = (buttonElement, config) => {
+  buttonElement.disabled = true;
+  buttonElement.classList.add(config.inactiveButtonClass);
+};
+
+const resetValidation = (formElement, inputList) => {
+  inputList.forEach((input) => {
+    hideInputError(formElement, input);
+  });
+};
+
+const setEventListener = (formElement, config) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(config.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement, config);
+
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = document.querySelectorAll(config.formSelector);
+  formList.forEach((formElement) => {
+    setEventListener(formElement, config);
+  });
+};
+
+enableValidation(settings);
